e16eeab498f7af06ea2f772eab89fd0b4b27474b
mariabernardes23 <maria.lopes10@estudante.ifms.edu.br>
2024-04-02 21:59:27 -0400

    código

---
+++ cliente.js
@@ -0,0 +1,50 @@
+const { log } = require('console');
+const net = require('net');
+const readline = require('readline');
+
+// Criar um cliente de socket
+const client = net.createConnection({
+  host: 'localhost',
+  port: 3001
+});
+
+// Configura a interface para leitura do terminal
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// Pergunta ao cliente e processa a entrada
+function writeMensage() {
+  rl.question('Digite sua mensagem: ', (input) => {
+    const mensagem = input;
+
+    if(mensagem.trim() == 'stop') {
+      // Fecha a conexão com o servidor
+      client.end();
+    } else {
+      pushMensage(mensagem);
+    }
+  });
+}
+
+// Lidar com mensagens do servidor
+function pullMensage() {
+  client.on('data', (mensagem) => {
+    console.log('-> Servidor:', mensagem.toString('utf8'));
+    writeMensage();
+  });
+}
+
+// Lidar com a desconexão do servidor
+client.on('close', () => {
+  console.log('Conexão fechada');
+});
+
+// Enviar uma mensagem para o servidor
+function pushMensage(mensagem) {
+  client.write(mensagem);
+  pullMensage();
+}
+
+pullMensage();
---
+++ servidor.js
@@ -0,0 +1,57 @@
+const { WriteStream } = require('fs');
+const net = require('net');
+const readline = require('readline');
+const porta = 3001;
+
+// Criar um servidor de socket
+const server = net.createServer((socket) => {
+  console.log('Cliente conectado');
+
+  // Configura a interface para a leitura do terminal
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  })
+
+  // Pergunta ao servidor e processa a entrada
+  function writeMensage() {
+    rl.question('Digite sua mensagem: ', (input) => {
+      const mensagem = input;
+
+      if(mensagem.trim() == 'stop') {
+        // Fecha a conexão com o cliente
+        socket.end()
+      } else {
+        pushMensage(mensagem)
+      }
+    })
+  }
+
+  // Enviar uma mensagem para o cliente
+  function pushMensage(mensagem) {
+    socket.write(mensagem)
+    pullMensage();
+  }
+
+  // Lidar com mensagens do cliente
+function pullMensage() {
+    socket.on('data', (mensagem) => {
+      if(mensagem) {
+        console.log('-> Cliente:', mensagem.toString('utf8'));
+      }
+      writeMensage()
+    });
+  }
+
+  // Lidar com a desconexão do cliente
+  socket.on('close', () => {
+    console.log('Conexão fechada');
+  });
+
+  writeMensage();
+});
+
+// Começar a escutar por novas conexões
+server.listen(porta, () => {
+  console.log(`Servidor executando na porta ${porta}`)
+});
